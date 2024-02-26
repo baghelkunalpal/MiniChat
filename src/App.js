275049ca9553e1082b031a337a89999bd371ceb0
kunal pal <kunalpal2292004@gmail.com>
2024-02-26 19:17:40 +0530

    Added a mini chat app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Register from "./pages/Register";
+// import Login from "./pages/Login";
+import './App.css'
+import './style.scss'
+import Home from "./pages/Home";
+const App = () => {
+  return (
+    <div>
+    <Home/>
+    </div>
+  )
+}
+
+export default App;
